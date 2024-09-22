f597b42329dc17158855b8cf31829cfa70ae1e9c
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-09-22 17:09:43 +0200

    styling menu

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -5,8 +5,6 @@
 
 
 
-// hamburger-menu.js
-
 document.addEventListener("DOMContentLoaded", function() {
     const hamburgerIcon = document.getElementById('hamburger-icon');
     const navMenu = document.getElementById('nav-dropdown-menu');
@@ -14,5 +12,13 @@ document.addEventListener("DOMContentLoaded", function() {
     
     hamburgerIcon.addEventListener('click', function() {
         navMenu.classList.toggle('active');
+    });
+
+   
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 1000) {
+            
+            navMenu.classList.remove('active');
+        }
     });
 });
